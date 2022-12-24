40c59b3754fbe1dae6031c65bc155dcbbb05c938
unknown <samarya@mcmaster.ca>
2022-12-24 17:33:05 -0500

    added hyper links to react and tailwindscss icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,12 @@ const Footer = () => {
     return (
         <footer className="fixed bottom-0 w-full flex flex-row justify-center items-center sm:pl-24 md:pl-32 lg:pl-64 bg-semi-transparent">
             <span className='mr-4'>Powered By</span>
-            <img className="h-8 mr-4" src={reactLogo} alt="React logo"/>
-            <img className="h-10" src={tailwindLogo} alt="TailwindsCSS logo"/>
+            <a href="https://reactjs.org/">
+                <img className="h-8 mr-4" src={reactLogo} alt="React logo"/>
+            </a>
+            <a href="https://tailwindcss.com/">
+                <img className="h-10" src={tailwindLogo} alt="TailwindsCSS logo"/>
+            </a>
         </footer>
     );
 }
