4fae99dcdce68cac7b020cf9622339e3bc280e4e
Sachin <samarya@mcmaster.ca>
2022-12-24 21:47:49 -0500

    react icon layout fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
     return (
         <footer className="fixed bottom-0 w-full flex flex-row justify-center items-center sm:pl-24 md:pl-32 lg:pl-64 bg-semi-transparent">
             <span className='mr-4'>Powered By</span>
-            <a href="https://reactjs.org/">
-                <img className="h-8 mr-4" src={reactLogo} alt="React logo"/>
+            <a className='mr-4' href="https://reactjs.org/">
+                <img className="h-8" src={reactLogo} alt="React logo"/>
             </a>
             <a href="https://tailwindcss.com/">
                 <img className="h-10" src={tailwindLogo} alt="TailwindsCSS logo"/>
