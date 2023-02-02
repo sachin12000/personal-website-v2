255d3a2c17a0028654a7ed42bcf563afd7303934
sachin12000 <sachins1872@gmail.com>
2023-02-02 14:43:51 -0500

    created an assets folder

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
-import tailwindLogo from '../Tailwind_logo.svg';
+import tailwindLogo from '../assets/Tailwind_logo.svg';
+import reactLogo from '../assets/React_logo.svg';
 
-import reactLogo from '../React_logo.svg';
 const Footer = () => {
     return (
         <footer className="fixed bottom-0 w-full flex flex-row justify-center items-center sm:pl-24 md:pl-32 lg:pl-64 bg-semi-transparent">
